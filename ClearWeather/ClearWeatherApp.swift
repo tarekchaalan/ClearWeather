05b5d89d9ee8dcd557cceeb0cad546460d83cad8
tarekchaalan <tchaalan23@icloud.com>
2024-12-28 02:22:03 -0800

    Initial Commit

---
+++ ClearWeather/ClearWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearWeatherApp.swift
+//  ClearWeather
+//
+//  Created by Tarek Chaalan on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ClearWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClearWeatherTests/ClearWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClearWeatherTests.swift
+//  ClearWeatherTests
+//
+//  Created by Tarek Chaalan on 12/28/24.
+//
+
+import Testing
+@testable import ClearWeather
+
+struct ClearWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
